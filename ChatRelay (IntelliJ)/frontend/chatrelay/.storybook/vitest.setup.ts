a8265659d35fc1da28679325a9a3903304f39ca5
Talhah Shaik <tshaik3@horizon.csueastbay.edu>
2025-05-18 21:29:59 -0700

    Update inital commit

--- ChatRelay (IntelliJ)/frontend/chatrelay/.storybook/vitest.setup.ts
+++ ChatRelay (IntelliJ)/frontend/chatrelay/.storybook/vitest.setup.ts
@@ -1,9 +1,11 @@
 import { beforeAll } from 'vitest';
 import { setProjectAnnotations } from '@storybook/sveltekit';
 import * as projectAnnotations from './preview';
+import * as addonAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-const project = setProjectAnnotations([projectAnnotations]);
-
-beforeAll(project.beforeAll);
+const annotations = setProjectAnnotations([projectAnnotations, addonAnnotations]);
+ 
+// Run Storybook's beforeAll hook
+beforeAll(annotations.beforeAll);
