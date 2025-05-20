4982662e8fe22cf3d7d17d99a60d446f43157a73
Talhah Shaik <tshaik3@horizon.csueastbay.edu>
2025-05-20 00:16:06 -0700

    Fixed DBManager to be final

--- ChatRelay (IntelliJ)/src/main/java/com/TShaik/ChatRelay/controller/LoginController.java
+++ ChatRelay (IntelliJ)/src/main/java/com/TShaik/ChatRelay/controller/LoginController.java
@@ -2,7 +2,10 @@ package com.TShaik.ChatRelay.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.TShaik.ChatRelay.models.LoginRequestData;
 
@@ -12,7 +15,7 @@ import chatRelay.DBManager;
 @RequestMapping("login")
 public class LoginController {
 
-    private DBManager dbManager = new DBManager("./", "Users.txt", "Chats.txt", "Messages.txt");
+    private final DBManager dbManager = new DBManager("./", "Users.txt", "Chats.txt", "Messages.txt");
 
     @PostMapping("/")
     public ResponseEntity<Boolean> getMessage(@RequestBody() LoginRequestData login) {
